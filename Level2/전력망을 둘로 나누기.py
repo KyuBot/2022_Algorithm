445f52eeb94eb7e5f8acb874372bce1532bbd9ac
kyubot <kpk03210724@gmail.com>
2022-01-28 21:14:31 +0900

    1solve

--- Level2/전력망을 둘로 나누기.py
+++ Level2/전력망을 둘로 나누기.py
@@ -1,3 +1,37 @@
 def solution(n, wires):
-    answer = -1
+    answer = 10**6
+
+    tmp = [[0] * (n + 1) for _ in range(n+1)]
+    chk = [[0] * (n + 1) for _ in range(n+1)]
+    for wire in wires:
+        tmp[wire[0]][wire[1]] = 1
+        tmp[wire[1]][wire[0]] = 1
+
+    def goCount(x):
+        nonlocal n
+        nonlocal tmp
+        nonlocal arr
+        for i in range(1, n+1):
+            if tmp[x][i] == 1 and arr[i] == 0:
+                arr[i] = 1
+                goCount(i)
+        return
+
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if tmp[i][j] == 1 and chk[i][j] == 0:
+                tmp[i][j] = 0
+                tmp[j][i] = 0
+                arr = [0] * (n + 1)
+                arr[i] = 1
+                goCount(i)
+                a = arr.count(1)
+                b = n - a
+                if abs(a-b) < answer:
+                    answer = abs(a-b)
+
+                tmp[i][j] = 1
+                tmp[j][i] = 1
+                chk[j][i] = 1
+
     return answer
